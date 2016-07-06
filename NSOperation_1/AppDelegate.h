b17c2fdf1463a72248d5cfdb1a7d2c184e2fd6a7
Ostrish <mac@ostrish.local>
2016-07-06 10:54:21 +0800

    Initial Commit

---
+++ NSOperation_1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSOperation_1
+//
+//  Created by Ostrish on 16/7/6.
+//  Copyright © 2016年 ZY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSOperation_1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSOperation_1
+//
+//  Created by Ostrish on 16/7/6.
+//  Copyright © 2016年 ZY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
